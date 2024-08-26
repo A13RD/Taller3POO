3b442fb70ea151301df76ff815008b62acf2643a
A13RD <alejandroruizdiaz0221@gmail.com>
2024-08-26 10:58:26 -0500

    __init__

---
+++ todo/__init__.py
@@ -0,0 +1,7 @@
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str, completed: bool = False, tags: list[str] = None):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = completed
+        self.tags = tags
